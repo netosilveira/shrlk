3af80a36b34456d929cd1ccfd6d9dc874ca88dd3
netosilveira <me@netosilveira.dev>
2021-11-13 16:36:03 -0300

    vue cli

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import store from './store'
-import router from './router'
 
-createApp(App).use(router).use(store).mount('#app')
+createApp(App).mount('#app')

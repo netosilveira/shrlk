d4d0dd691660a5d7cc0442a174f806812748025d
netosilveira <me@netosilveira.dev>
2021-11-16 20:02:51 -0300

    using v-for

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="app">
-  <p>{{ juices[0].name}}</p>
+  <p v-for="juice in juices" :key="juice.id">{{ juice.name }}  {{ juice.price }}</p>
   </div>
 </template>
 
@@ -15,7 +15,7 @@ export default defineComponent({
     const juices = ref<Juice[]>([
       { name: 'Morango com kiwi', flavors: ['morango', 'kiwi', 'limao'], price: 20, id: 1 },
       { name: 'Abacaxi com limao', flavors: ['abacaxi', 'limao', 'ice'], price: 20, id: 2 },
-      { name: 'Uva ice', flavors: ['uva', 'cereja', 'ice'], price: 20, id: 3 }
+      { name: 'Uva Gelada', flavors: ['uva', 'cereja', 'ice'], price: 20, id: 3 }
     ])
 
     return { juices }

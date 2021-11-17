21e745523b22fc8bb0eede5e12fcbf450444ef20
Neto Silveira <me@netosilveira.dev>
2021-11-16 23:59:50 -0300

    v-for test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
 <div class="app">
-  <p v-for="juice in juices" :key="juice.id">{{ juice.name }}  {{ juice.price }}</p>
+  <!-- <p v-for="juice in juices" :key="juice.id">
+    {{ juice.name }} 
+    
+    <a v-for="flavor in juice.flavors" :key="flavor.id"> 
+      {{ flavor }} </a>
+      
+    {{ juice.price }}</p> -->
+
+  <p v-for="juice in juices" :key="juice.id"> {{ juices[juice.id - 1].id }}. {{juices[juice.id -1].name}} - R${{juices[juice.id -1].price}}</p>
   </div>
 </template>
 
@@ -14,14 +22,16 @@ export default defineComponent({
   setup() {
     const juices = ref<Juice[]>([
       { name: 'Morango com kiwi', flavors: ['morango', 'kiwi', 'limao'], price: 20, id: 1 },
-      { name: 'Abacaxi com limao', flavors: ['abacaxi', 'limao', 'ice'], price: 20, id: 2 },
-      { name: 'Uva Gelada', flavors: ['uva', 'cereja', 'ice'], price: 20, id: 3 }
+      { name: 'Abacaxi com limao', flavors: ['abacaxi', 'limao', 'gelo'], price: 20, id: 2 },
+      { name: 'Uva Gelada', flavors: ['uva', 'cereja', 'gelo'], price: 20, id: 3 }
     ])
 
     return { juices }
   },
   methods: {
-
+    getJuice(name : string){
+      console.log(this.juices.find(juice => juice.name == name));
+    }
   }
 });
 </script>
